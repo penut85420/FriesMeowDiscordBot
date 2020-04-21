34b759b2d50eb5f67ebdc1a7a9b7c3aef5d91da7
PenutChen <penut85420@gmail.com>
2020-04-21 14:05:41 +0800

    Fix test case in dice_test.py

--- tests/dice_test.py
+++ tests/dice_test.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('.')
 import unittest
 from modules.dice import Dice, DiceFormatError, DiceRangeError
 
@@ -16,7 +18,7 @@ class DiceTest(unittest.TestCase, Dice):
         self.assertEqual(Dice.roller('51d100'), '第 1 個參數超過最大值 50')
         self.assertEqual(Dice.roller('50d1001'), '第 2 個參數超過最大值 1000')
         self.assertEqual(Dice.roller('50d1000+100000'), '第 3 個參數超過最大值 99999')
-        self.assertEqual(Dice.roller('3dd6'), '格式必須是 NdM 或 NdM+K')
+        self.assertEqual(Dice.roller('3dd6'), '格式必須是 NdM 或 NdM+K，例如 2d6 是投擲兩顆六面的骰子')
 
     def test_parse(self):
         self.assertEqual(self._parse('3d6'), (3, 6, 0, '', 0))
